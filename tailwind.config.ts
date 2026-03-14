b19b55e781b51a227b25bc5698df0b35c1bbf24a
Cengoringo <tevfikcengizoral@gmail.com>
2026-03-14 15:24:10 +0300

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2563EB",
+        appbg: "#F8F9FA",
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
